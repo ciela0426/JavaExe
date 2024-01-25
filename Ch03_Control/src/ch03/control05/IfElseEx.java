b7e888e958fac4a33f89559a809d4ce7e728913e
Orbit <ciela0426@naver.com>
2024-01-25 15:36:21 +0900

    switch 문자열 비교 예

--- Ch03_Control/src/ch03/control05/IfElseEx.java
+++ Ch03_Control/src/ch03/control05/IfElseEx.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class IfElseEx {
 	public static void main(String[] args) {
 
-		System.out.println("1. 짜장면");
-		System.out.println("2. 짬뽕");
-		System.out.println("3. 탕수육");
-		System.out.println("4. 유산슬");
-		System.out.println("5. 유린기");
+		System.out.println("1. 봄");
+		System.out.println("2. 여름");
+		System.out.println("3. 가을");
+		System.out.println("4. 겨울");
 		System.out.print("선택 >> ");
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int sel = sc.nextInt();
+		String sel = sc.nextLine();
 		
-		if (sel == 1) 
-			System.out.println("짜장면이 나왔습니다.");
-		 else if (sel == 2) 
-			System.out.println("짬뽕이 나왔습니다.");
-		 else if (sel == 3) 
-			System.out.println("탕수육이 나왔습니다.");
-		 else if (sel == 4) 
-			System.out.println("유산슬이 나왔습니다.");
-		 else if (sel == 5) 
-			System.out.println("유린기가 나왔습니다.");
-		 else 
-			System.out.println("해당 메뉴 번호가 없습니다.");
+		switch(sel) {
+		case "봄":
+			System.out.println("새싹이 자라난다");
+			break;
+		case "여름":
+			System.out.println("산과 바다로 가자");
+			break;
+		case "가을":
+			System.out.println("책을 읽자");
+			break;
+		case "겨울":
+			System.out.println("스키장에 간다");
+			break;
+		}
 		
 		sc.close();
 	}
