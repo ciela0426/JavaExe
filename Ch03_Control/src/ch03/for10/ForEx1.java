24b96bde54d10d96b1fcc253de93f7a7cc244018
Orbit <ciela0426@naver.com>
2024-01-25 17:16:04 +0900

    for문 신

--- Ch03_Control/src/ch03/for10/ForEx1.java
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -3,10 +3,11 @@ package ch03.for10;
 public class ForEx1 {
 	public static void main(String[] args) {
 		// 1~10까지 출력
-		int i = 1;
-		for (;i <= 10;) {
+		
+		// 1번만 실행; 조건(계속 비교); 실행이 끝나면 변화
+		// 시작; 끝; 변화량
+		for(int i = 1; i<=10; i++) {
 			System.out.println(i);
-			i++;
 		}
 	}
 }
