088c03e8ce105b0a838a82156563e27ad9b81d51
Orbit <ciela0426@naver.com>
2024-01-25 17:09:36 +0900

    문제풀이 지나간 것 + for문 기본

--- Ch03_Control/src/ch03/while08/Resolve2.java
+++ Ch03_Control/src/ch03/while08/Resolve2.java
@@ -4,8 +4,8 @@ public class Resolve2 {
 	public static void main(String[] args) {
 		int i = 10; 
 		while(i < 20) { 
-			i++; 
 			System.out.print(i + ", ");
+			i++; 
 		}
 	}
 }
--- Ch03_Control/src/ch03/while08/Resolve3.java
+++ Ch03_Control/src/ch03/while08/Resolve3.java
@@ -1,5 +1,6 @@
 package ch03.while08;
 
+//3. while문을 이용해서 1부터 10까지 합을 출력하세요
 public class Resolve3 {
 	public static void main(String[] args) {
 		int i = 1, sum = 0; 
@@ -9,6 +10,6 @@ public class Resolve3 {
 			System.out.println();
 			i++;
 		}
-		System.out.println("1~10의 합은 " : );
+		System.out.println("1~10의 합은 " + sum);
 	}
 }
